a7f99ed11fd98ba4098d1760e107c4b3ba908759
Yuri <peterycis@gmail.com>
2023-06-30 22:59:19 +0100

    name jeff

--- src/Button.java
+++ src/Button.java
@@ -4,7 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Button extends JButton implements ActionListener {
-    private final DisplayOutput displayOutput;
+    DisplayOutput displayOutput;
 
     Button(String text, DisplayOutput displayOutput) {
         super(text);
